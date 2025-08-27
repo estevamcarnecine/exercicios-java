ab1d9dbe58c6724af0743d057e54614a50d5f60b
Estevam Carnecine Silva <estvmc@gmail.com>
2025-08-27 20:49:30 -0300

    uri1003

---
+++ src/uri1003.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class uri1003 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y, sum;
+		
+	
+		x = sc.nextInt();
+		y = sc.nextInt();
+		sum = x + y;
+		
+		System.out.println("SOMA = " +sum);
+		
+		sc.close();
+		
+	}
+
+}
